1c95a9c5096fab3e050f8f7f7ff78825bccab6be
Farid Sakhizad <fsakhyzadcf@gmail.com>
2025-11-07 19:38:49 -0700

    Initial Commit

---
+++ pg1/pg1App.swift
@@ -0,0 +1,17 @@
+//
+//  pg1App.swift
+//  pg1
+//
+//  Created by Farid Sakhizad on 07.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct pg1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
